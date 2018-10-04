202f8a14551dbad8212735fc6e4ffd372f86d65a
Cooper Pellaton <cooper@cooperpellaton.com>
2018-10-04 17:09:23 -0400

    [Update] Remove unnecessary files.

--- preproc_backup_072817/paths.sh
+++
@@ -1,8 +0,0 @@
-#----------------------------------------------------------------------
-# Paths for necessary programs. If you update any of the below programs
-# be sure to update the path here, or make a symbolic link in to the 
-# program titled with the below paths.
-#----------------------------------------------------------------------
-medcon=/usr/local/xmedcon/bin/medcon
-afni=usr/local/afni/bin/afni
-preproc=/home/mw/wheeler_shared/usb3/home/jt/scripts/preproc
